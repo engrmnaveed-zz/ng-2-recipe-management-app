d0a2807414b3bdeb0efcc1d9ff23b4ac24070f13
muhammad-naveed <m.naveed.ch@gmail.com>
2017-07-05 23:16:41 +0500

    Some basic navigation added using Custom Events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Recipe Manager';
+  loadedView = 'recipe';
+
+  onNavigate(selectedView: string) {
+    this.loadedView = selectedView;
+  }
 }

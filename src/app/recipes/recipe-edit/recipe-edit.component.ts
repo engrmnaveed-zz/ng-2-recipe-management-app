2b3ba79544599c0bfce169d4485ddbf407779d00
muhammad-naveed <m.naveed.ch@gmail.com>
2017-07-13 03:57:36 +0500

    minor fix

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -52,7 +52,7 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onDeleteIngredient(index: number) {
-    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    (<FormArray>this.recipeForm.get('ingrediants')).removeAt(index);
   }
 
   onCancel() {
